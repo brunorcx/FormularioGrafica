04662b0dcf6e583d535ab7793ed6281a5109a962
Bruno RC <brunorcx@hotmail.com>
2020-02-05 16:41:41 -0400

    Formatação e teste community

--- Form1.cs
+++ Form1.cs
@@ -9,81 +9,65 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FormularioGrafica {
+
     public partial class Form1 : Form {
+
         public Form1() {
             InitializeComponent();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e) {
-         
         }
 
         private void label1_Click(object sender, EventArgs e) {
-
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
-
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox7_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox6_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox8_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e) {
-
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e) {
-
         }
 
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e) {
-
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e) {
-
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e) {
-
         }
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e) {
-
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -107,4 +91,5 @@ namespace FormularioGrafica {
         }
     }
 }
+
 // Site ícones https://www.flaticon.com/free-icon/home_25694
