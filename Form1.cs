a50398db64fac0341ee190ae9e883867d13f08ad
Bruno RC <brunorcx@hotmail.com>
2020-02-04 22:40:15 -0400

    Teste Master

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace FormularioGrafica {
         private void textBox1_TextChanged(object sender, EventArgs e) {
             int a;
             a = 1;
+            a = a + 2;
         }
 
         private void label1_Click(object sender, EventArgs e) {
