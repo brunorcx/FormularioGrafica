17815aca9df73d15cfd96423741399cf72e4d739
Bruno RC <brunorcx@hotmail.com>
2020-02-10 12:21:32 -0400

    Login e Senha implementados Conexão com o banco funcionando

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -23,36 +23,72 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // button_Login
             // 
-            this.button1.Location = new System.Drawing.Point(214, 147);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.button_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Login.Location = new System.Drawing.Point(202, 208);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(267, 34);
+            this.button_Login.TabIndex = 0;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = false;
+            this.button_Login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsuario.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxUsuario.Location = new System.Drawing.Point(202, 122);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(267, 24);
+            this.textBoxUsuario.TabIndex = 2;
+            this.textBoxUsuario.Text = "Usuário";
+            this.textBoxUsuario.TextChanged += new System.EventHandler(this.textBoxUsuario_TextChanged);
+            this.textBoxUsuario.Enter += new System.EventHandler(this.Usuario_Enter);
+            this.textBoxUsuario.Leave += new System.EventHandler(this.Usuario_Leave);
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSenha.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxSenha.Location = new System.Drawing.Point(202, 163);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(267, 24);
+            this.textBoxSenha.TabIndex = 3;
+            this.textBoxSenha.Text = "Senha";
+            this.textBoxSenha.Enter += new System.EventHandler(this.Senha_Enter);
+            this.textBoxSenha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Senha_KeyDown);
+            this.textBoxSenha.Leave += new System.EventHandler(this.Senha_Leave);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(744, 346);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.button_Login);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxSenha;
     }
 }
--- Login.cs
+++ Login.cs
@@ -12,24 +12,71 @@ using System.Windows.Forms;
 namespace FormularioGrafica {
 
     public partial class Login : Form {
+        private string login;
+        private string senha;
 
         public Login() {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            this.Hide();
-            // Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
-            //string connetionString;
-            //SqlConnection cnn;
-            //connetionString = @"Server=localhost\SQLEXPRESS;Database=master;Initial Catalog=banco1;User ID=mario;Password=123123";
-            //cnn = new SqlConnection(connetionString);
-            //cnn.Open();
-            //MessageBox.Show("Connection Open  !");
-            //cnn.Close();
+            DBConnect dB = new DBConnect();
+            List<string>[] list = new List<string>[2];
 
-            Form1 form = new Form1();
-            form.Show();
+            list = dB.Select("SELECT Nome,Senha FROM funcionarios WHERE (Nome =\'" + login + "\') AND (Senha =\'" + senha + "\')");
+            if (list[0].Count() == 0) {
+                MessageBox.Show("Login ou Senha incorretos");
+            }
+            else {
+                this.Hide();
+                Form1 form = new Form1();
+                form.Show();
+            }
+            //foreach (var el in list) {
+            //    buscaLogin = el[0];
+            //}
+            //dB.Insert("INSERT INTO funcionarios (CPF, Nome,Senha) VALUES(04548965149, 'Mario', 'minhaSenha')");
+        }
+
+        private void Usuario_Enter(object sender, EventArgs e) {
+            if (textBoxUsuario.Text == "Usuário") {
+                textBoxUsuario.Text = "";
+                textBoxUsuario.ForeColor = Color.FromArgb(62, 120, 138);
+            }
+        }
+
+        private void Usuario_Leave(object sender, EventArgs e) {
+            if (textBoxUsuario.Text == "") {
+                textBoxUsuario.Text = "Usuário";
+                textBoxUsuario.ForeColor = SystemColors.InactiveCaption;
+            }
+            login = textBoxUsuario.Text;
+        }
+
+        private void Senha_Enter(object sender, EventArgs e) {
+            if (textBoxSenha.Text == "Senha") {
+                textBoxSenha.Text = "";
+                textBoxSenha.ForeColor = Color.FromArgb(62, 120, 138);
+            }
+        }
+
+        private void Senha_Leave(object sender, EventArgs e) {
+            if (textBoxSenha.Text == "") {
+                textBoxSenha.Text = "Senha";
+                textBoxSenha.ForeColor = SystemColors.InactiveCaption;
+            }
+            senha = textBoxSenha.Text;
+        }
+
+        private void Senha_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                senha = textBoxSenha.Text;
+                button_Login.PerformClick();
+            }
+
+        }
+
+        private void textBoxUsuario_TextChanged(object sender, EventArgs e) {
         }
     }
 }
