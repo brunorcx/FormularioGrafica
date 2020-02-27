7f38771770071779ad06fec44763f6aa00386864
Bruno RC <brunorcx@hotmail.com>
2020-02-27 12:04:05 -0400

    Criada 10 colunas pra centralização de colunas com merge. Adicionado linha 7 e retirado bordas onde se tem shade

--- TratamentoPDF.cs
+++ TratamentoPDF.cs
@@ -62,9 +62,17 @@ namespace FormularioGrafica {
             Table table = new Table();
             table.Borders.Width = 0.75;
             table.Borders.Color = Colors.DarkRed;
-            Column column = table.AddColumn(Unit.FromCentimeter(15));//Primeira coluna
+            Column column = table.AddColumn(Unit.FromCentimeter(2));//Primeira coluna
             //column.Format.Alignment = ParagraphAlignment.Center;
-            table.AddColumn(Unit.FromCentimeter(5));//Segunda coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Segunda coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Terceira coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Quarta coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Quinta coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Sexta coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Sétima coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Oitava coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Nona coluna
+            table.AddColumn(Unit.FromCentimeter(2));//Decima coluna
             //Arrastar tabela
             table.Rows.LeftIndent = "-2cm";
             table.Rows.Height = 35;
@@ -77,10 +85,11 @@ namespace FormularioGrafica {
 
             cell.AddParagraph("ENTRADA:\t\t\t\t\tENTREGA:"); //113 caracteres
             cell.VerticalAlignment = VerticalAlignment.Bottom;
-            cell = row.Cells[1];
+            cell = row.Cells[9];
             cell.Format.Alignment = ParagraphAlignment.Center;
             cell.VerticalAlignment = VerticalAlignment.Center;
             cell.AddParagraph("ATENDENTE \n Nº 0001");
+            row.Cells[0].MergeRight = 8;
             //Linha 1
             row = table.AddRow();
             //row.Borders.Color = Colors.White;
@@ -94,7 +103,7 @@ namespace FormularioGrafica {
             cell.Format.Borders.Bottom.Color = Colors.DarkRed;
             cell.AddParagraph("CNPJ:\t\t\t\tFONE:").Format.Borders.Bottom.Color = Colors.White;
             cell.VerticalAlignment = VerticalAlignment.Bottom;
-            row.Cells[0].MergeRight = 1;
+            row.Cells[0].MergeRight = 9;
 
             //cell = row.Cells[1];
             //cell.AddParagraph("texto 1");
@@ -102,9 +111,10 @@ namespace FormularioGrafica {
             row = table.AddRow();
             cell = row.Cells[0];
             cell.AddParagraph("Serviço:");
-            cell = row.Cells[1];
+            cell = row.Cells[9];
             cell.AddParagraph("Valor R$").Format.Alignment = ParagraphAlignment.Center;
             cell.AddParagraph("100,00").Format.Alignment = ParagraphAlignment.Center;
+            row.Cells[0].MergeRight = 8;
             //Linha 3
             row = table.AddRow();
             cell = row.Cells[0];
@@ -136,12 +146,13 @@ namespace FormularioGrafica {
             paragraph.Format.Borders.Bottom.Color = Colors.DarkRed;
             cell.Add(paragraph);
 
-            cell = row.Cells[1];
+            cell = row.Cells[9];
             cell.AddParagraph("TOTAL:").Format.Font.Size = 11;
-
+            row.Cells[0].MergeRight = 8;
             //Linha 4
             row = table.AddRow();
             row.Shading.Color = Colors.Crimson;
+            row.Borders.Bottom.Clear();
             cell = row.Cells[0];
             cell.Format.Font.Color = Colors.White;
             cell.Format.Font.Name = "Arial";
@@ -151,26 +162,48 @@ namespace FormularioGrafica {
             cell.AddParagraph("O CLIENTE ASSUME A RESPONSABILIDADE DO PEDIDO APÓS A CONFIRMAÇÃO DO MODELO, E PAGAMENTO " +
                 "SOB O VALOR ASSINADO, APOS A CONFIRMAÇÃO. NÃO TRABALHAMOS COM PROVA DE COR, POR ISSO PODEM OCORRER VARIAÇÕES MÍNIMAS" +
                 "DE COR DURANTE O PROCESSO DE IMPRESSÃO.").Format.Font.Size = 7;
-            row.Cells[0].MergeRight = 1;
+            row.Cells[0].MergeRight = 9;
 
             //Linha 5
             row = table.AddRow();
+            row.Borders.Top.Clear();
             cell = row.Cells[0];
             cell.Format.Alignment = ParagraphAlignment.Center;
             cell.VerticalAlignment = VerticalAlignment.Bottom;
             cell.AddParagraph("_________________________________").Format.Font.Color = Colors.DarkRed;
             cell.AddParagraph("CLIENTE");
-            row.Cells[0].MergeRight = 1;
+            row.Cells[0].MergeRight = 9;
 
             //Linha 6
-            row = table.AddRow();
-            cell = row.Cells[0];
-            cell.Format.Alignment = ParagraphAlignment.Center;
-            cell.VerticalAlignment = VerticalAlignment.Center;
-            cell.AddParagraph("ENTRADA\t\t\t\t\t\t\tRESTANTE");
-            cell.AddParagraph("R$ 50,00\t\t\t\t\t\t\tR$ 50,00"); // TODO Colocar uma variavel para indicar  entrade e restante
-            row.Cells[0].MergeRight = 1;
 
+            row = table.AddRow();
+
+            row.Shading.Color = Colors.Crimson;
+            row.Format.Font.Bold = true;
+            row.Format.Font.Color = Colors.White;
+            row.Format.Alignment = ParagraphAlignment.Center;
+            row.VerticalAlignment = VerticalAlignment.Top;
+            row.Borders.Bottom.Clear();
+            cell = row.Cells[0];
+            //cell.Format.Shading.Color = Colors.Crimson;
+            cell.AddParagraph("ENTRADA");
+            cell = row.Cells[5];
+            cell.AddParagraph("RESTANTE");
+            row.Cells[0].MergeRight = 4;
+            row.Cells[5].MergeRight = 4;
+
+            //Linha 7
+            row = table.AddRow();
+            //row.Format.Font.Color = Colors.DarkRed;
+            row.Format.Alignment = ParagraphAlignment.Center;
+            row.VerticalAlignment = VerticalAlignment.Top;
+            row.Borders.Top.Clear();
+            cell = row.Cells[0];
+            cell.AddParagraph("R$ 50,00"); // TODO Colocar uma variavel para indicar  entrada e restante
+            cell = row.Cells[5];
+            cell.AddParagraph("R$ 50,00");
+            row.Cells[0].MergeRight = 4;
+            row.Cells[5].MergeRight = 4;
             //table.SetEdge(0, 0, 2, 3, Edge.Box, BorderStyle.Single, 1.5, Colors.DarkRed);
             //table.LeftPadding = 60;
 
